47af527768f5c5fb50b91d5d00fd098eaafddff3
= <=>
2023-12-09 16:38:33 +0100

    2 Commit

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.gitdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import com.example.gitdemo.ui.theme.GitDemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("TAG", "Esto es una prueba")
         setContent {
             GitDemoTheme {
                 // A surface container using the 'background' color from the theme
